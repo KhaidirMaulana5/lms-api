5cd8b394c4dad2a1a6b20350adaa5fb08a3c76be
KhaidirMaulana5 <126649295+KhaidirMaulana5@users.noreply.github.com>
2023-05-31 16:03:17 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\BookController;
+use App\Http\Controllers\Api\V1\MemberController;
 
 /*
 |--------------------------------------------------------------------------
